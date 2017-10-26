0c2658dbc3b2606054b290857ff453de3a21ed8d
Akshay <patwardhanakshay8@gmail.com>
2017-10-26 14:25:09 +0530

    Second commit

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use((req,res,next) => {
     next();    
 });
 
-app.use((req,res,next) => {
-    res.render('maintainance.hbs');
-});
+// app.use((req,res,next) => {
+//     res.render('maintainance.hbs');
+// });
 
 hbs.registerHelper('getFullYear', () => {
     return new Date().getFullYear();
